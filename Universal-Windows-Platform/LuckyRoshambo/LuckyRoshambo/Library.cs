283382d6b46d1f28c0fafe0729227e28c49c4639
RoguePlanetoid <rogueplanetoid@hotmail.com>
2019-07-17 21:16:05 +0100

    Lucky Dominoes, Lucky Roshambo, Cards Game, Touch Game

---
+++ Universal-Windows-Platform/LuckyRoshambo/LuckyRoshambo/Library.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+public class Library
+{
+    private const int size = 3;
+    private const int lost = 0;
+    private const int win = 1;
+    private const int draw = 2;
+    private readonly int[,] match =
+        new int[size, size]
+    {
+        { draw, lost, win },
+        { win, draw, lost },
+        { lost, win, draw }
+    };
+    private readonly string[] options = new string[]
+    {
+        "\U0000270A", "\U0000270B", "\U0000270C"
+    };
+    private readonly string[] values = new string[]
+    {
+        "You Lost!", "You Win!", "You Draw!"
+    };
+
+    private Random _random = new Random((int)DateTime.Now.Ticks);
+
+    private Viewbox Option(int value)
+    {
+        TextBlock textblock = new TextBlock()
+        {
+            Text = options[value],
+            IsColorFontEnabled = true,
+            Margin = new Thickness(2),
+            TextLineBounds = TextLineBounds.Tight,
+            FontFamily = new FontFamily("Segoe UI Emoji"),
+            HorizontalTextAlignment = TextAlignment.Center
+        };
+        return new Viewbox()
+        {
+            Child = textblock
+        };
+    }
+
+    private async void Show(string title, int option)
+    {
+        ContentDialog dialog = new ContentDialog()
+        {
+            Title = title,
+            PrimaryButtonText = "OK",
+            Content = new Viewbox()
+            {
+                Width = 150,
+                Height = 150,
+                Child = Option(option)
+            }
+        };
+        await dialog.ShowAsync();
+    }
+
+    private void Choose(int player)
+    {
+        int computer = _random.Next(0, size - 1);
+        int result = match[player, computer];
+        Show($"Computer Picked - {values[result]}", computer);
+    }
+
+    private void Add(StackPanel panel, int option)
+    {
+        Button button = new Button()
+        {
+            Width = 200,
+            Height = 200,
+            Tag = option,
+            Content = Option(option),
+            Margin = new Thickness(5)
+        };
+        button.Click += (object sender, RoutedEventArgs e) =>
+        {
+            button = (Button)sender;
+            Choose((int)button.Tag);
+        };
+        panel.Children.Add(button);
+    }
+
+    private void Layout(Grid grid)
+    {
+        grid.Children.Clear();
+        StackPanel panel = new StackPanel()
+        {
+            Orientation = Orientation.Horizontal,
+            HorizontalAlignment = HorizontalAlignment.Center
+        };
+        for (int i = 0; i < size; i++)
+        {
+            Add(panel, i);
+        }
+        grid.Children.Add(panel);
+    }
+
+    public void New(Grid grid)
+    {
+        Layout(grid);
+    }
+}
---
+++ Universal-Windows-Platform/TouchGame/TouchGame/Library.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+public class Library
+{
+    private const string title = "Touch Game";
+    private const int size = 2;
+    private const int speed = 800;
+    private const int light = 400;
+    private const int click = 200;
+    private const int level = 100;
+    private readonly Dictionary<int, string> _square = 
+        new Dictionary<int, string>()
+    {
+        { 0, "\U0001F7E5" }, // Red Square
+        { 1, "\U0001F7E6" }, // Blue Square
+        { 2, "\U0001F7E9" }, // Green Square
+        { 3, "\U0001F7E8" }, // Yellow Square
+    };
+
+    private int _turn = 0;
+    private int _count = 0;
+    private bool _play = false;
+    private bool _isTimer = false;
+    private List<int> _items = new List<int>();
+    private DispatcherTimer _timer = new DispatcherTimer();
+    private Random _random = new Random((int)DateTime.Now.Ticks);
+
+    public void Show(string content, string title)
+    {
+        _ = new MessageDialog(content, title).ShowAsync();
+    }
+
+    private List<int> Choose(int start, int finish, int total)
+    {
+        int number;
+        List<int> numbers = new List<int>();
+        while (numbers.Count < total) // Select Numbers
+        {
+            // Random non-unique Number between Start and Finish
+            number = _random.Next(start, finish + 1);
+            numbers.Add(number); // Add Number
+        }
+        return numbers;
+    }
+
+    private Viewbox Square(int value)
+    {
+        TextBlock textblock = new TextBlock()
+        {
+            Text = _square[value],
+            IsColorFontEnabled = true,
+            TextLineBounds = TextLineBounds.Tight,
+            FontFamily = new FontFamily("Segoe UI Emoji"),
+            HorizontalTextAlignment = TextAlignment.Center
+        };
+        return new Viewbox()
+        {
+            Child = textblock
+        };
+    }
+
+    private void Score(int value)
+    {
+        if (value == _items[_count])
+        {
+            if (_count < _turn)
+            {
+                _count++;
+            }
+            else
+            {
+                _isTimer = true;
+                _play = false;
+                _count = 0;
+                _turn++;
+            }
+        }
+        else
+        {
+            Show($"Game Over! You scored {_turn}!", title);
+            _isTimer = false;
+            _play = false;
+            _count = 0;
+            _turn = 0;
+            _timer.Stop();
+        }
+    }
+
+    private void Set(Grid grid, int value, int period)
+    {
+        Button button = (Button)grid.Children.Single(s =>
+            (int)((Button)s).Tag == value);
+        button.Opacity = 0.25;
+        DispatcherTimer opacity = new DispatcherTimer()
+        {
+            Interval = TimeSpan.FromMilliseconds(period)
+        };
+        opacity.Tick += (object sender, object e) =>
+        {
+            button.Opacity = 1.0;
+            opacity.Stop();
+        };
+        opacity.Start();
+    }
+
+    private void Tick(Grid grid)
+    {
+        if (_isTimer)
+        {
+            if (_count <= _turn)
+            {
+                Set(grid, _items[_count], light);
+                _count++;
+            }
+            if (_count > _turn)
+            {
+                _isTimer = false;
+                _play = true;
+                _count = 0;
+            }
+        }
+    }
+
+    private void Add(Grid grid, int row, int column, int count)
+    {
+        Button button = new Button()
+        {
+            Tag = count,
+            Width = 100,
+            Height = 100,
+            Content = Square(count),
+            Margin = new Thickness(5)
+        };
+        button.Click += (object sender, RoutedEventArgs e) =>
+        {
+            if (_play)
+            {
+                int value = (int)((Button)sender).Tag;
+                Set(grid, value, click);
+                Score(value);
+            }
+        };
+        button.SetValue(Grid.ColumnProperty, column);
+        button.SetValue(Grid.RowProperty, row);
+        grid.Children.Add(button);
+    }
+
+    private void Layout(ref Grid grid)
+    {
+        grid.Children.Clear();
+        grid.RowDefinitions.Clear();
+        grid.ColumnDefinitions.Clear();
+        // Setup Grid
+        for (int index = 0; (index < size); index++)
+        {
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+        }
+        int count = 0;
+        // Setup Board
+        for (int column = 0; (column < size); column++)
+        {
+            for (int row = 0; (row < size); row++)
+            {
+                Add(grid, row, column, count);
+                count++;
+            }
+        }
+    }
+
+    public void New(Grid grid)
+    {
+        Layout(ref grid);
+        _items = Choose(0, 3, level);
+        _play = false;
+        _turn = 0;
+        _count = 0;
+        _isTimer = true;
+        _timer = new DispatcherTimer
+        {
+            Interval = TimeSpan.FromMilliseconds(speed)
+        };
+        _timer.Tick += (object sender, object e) =>
+        {
+            Tick(grid);
+        };
+        _timer.Start();
+    }
+}
