c3c9fee2db2c20b757e893867eb949ba92f31fc8
RoguePlanetoid <rogueplanetoid@hotmail.com>
2020-04-05 18:24:25 +0100

    Spotify for Developers Workshop Source Code

---
+++ Spotify-for-Developers/SpotifyForDevelopers/Result.cs
@@ -0,0 +1,19 @@
+﻿public class Result
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Image { get; set; }
+    public Result Inner { get; set; }
+
+    public Result(
+        string id = null,
+        string name = null,
+        string image = null,
+        Result inner = null)
+    {
+        Id = id;
+        Name = name;
+        Image = image;
+        Inner = inner;
+    }
+}
